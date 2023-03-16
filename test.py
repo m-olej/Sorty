251ff7e03b03b10f988ebfa7e8c6a0d4dbda0dd9
Olej <music.olejniczak@wp.pl>
2023-03-16 17:19:28 +0100

    iter_QS

--- test.py
+++ test.py
@@ -1,8 +1,39 @@
-# from GeneratorDanych import *
-# from sorts import *
-# from main import *
-from matplotlib import pyplot as plt
+from GeneratorDanych import *
 
-# listDict, sedgewickList = generator(10)
 
-print(1e4*2)
+listDict = generator(10000)
+
+qArray = listDict["descendList"][:]
+
+def iterative_quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    stack = [(0, len(arr)-1)]
+    
+    while stack:
+        left, right = stack.pop()
+        pivot = partition(arr, left, right)
+        
+        if pivot - 1 > left:
+            stack.append((left, pivot-1))
+        if pivot + 1 < right:
+            stack.append((pivot+1, right))
+            
+    return arr
+        
+def partition(arr, left, right):
+    pivot = arr[right]
+    i = left - 1
+    
+    for j in range(left, right):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[right] = arr[right], arr[i+1]
+    
+    return i+1
+
+
+print(iterative_quicksort(qArray))
